925842bc260af77485b0a98d5591a5d4a5130b91
Túlio Paim <tutpaim@gmail.com>
2021-09-30 19:41:39 -0300

    Cria ICommandHandler em Core

---
+++ CQRS.Core/Application/Interfaces/ICommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace CQRS.Core.Application.Interfaces
+{
+    public interface ICommandHandler<in TCommandInput, TCommandResult> : IRequestHandler<TCommandInput, TCommandResult>
+        where TCommandInput : IRequest<TCommandResult>, IMediatorInput<TCommandResult>
+        where TCommandResult : IMediatorResult
+    {
+        new Task<TCommandResult> Handle(TCommandInput command, CancellationToken cancellationToken);
+    }
+}

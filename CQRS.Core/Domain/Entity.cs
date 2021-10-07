11f6c4194581095e448e68aad6af7a5828263d20
Túlio Paim <tutpaim@gmail.com>
2021-10-07 18:41:09 -0300

    Cria Entity base

---
+++ CQRS.Core/Domain/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CQRS.Core.Domain
+{
+    public class Entity : IEntity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Entity(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+}

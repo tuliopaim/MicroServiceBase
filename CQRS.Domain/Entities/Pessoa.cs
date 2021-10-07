9dd36ff9054d3c53d7ea0a83f187f02bd5c38403
Túlio Paim <tutpaim@gmail.com>
2021-10-07 18:41:33 -0300

    Cria entidade Pessoa

---
+++ CQRS.Domain/Entities/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using CQRS.Core.Domain;
+
+namespace CQRS.Domain.Entities
+{
+    public class Pessoa : Entity
+    {
+        public Pessoa(string nome, byte idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public string Nome { get; private set; }
+        public byte Idade { get; private set; }
+
+        public void AlterarNome(string novoNome)
+        {
+            if (string.IsNullOrWhiteSpace(novoNome)) return;
+
+            Nome = novoNome;
+        }
+
+        public void AlterarIdade(byte novaIdade)
+        {
+            if (novaIdade < 18) return;
+
+            Idade = novaIdade;
+        }
+    }
+}

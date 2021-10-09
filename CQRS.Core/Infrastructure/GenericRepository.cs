742baedd099c1c3eca029676f6958bb2ed25e634
Túlio Paim <tutpaim@gmail.com>
2021-10-09 17:06:40 -0300

    Cria repositorio generico

---
+++ CQRS.Core/Infrastructure/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using CQRS.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS.Core.Infrastructure
+{
+    public abstract class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : Entity
+    {
+        private readonly EfDbContext _context;
+
+        protected GenericRepository(EfDbContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public IQueryable<TEntity> Get()
+        {
+            var query = _context
+                .Set<TEntity>()
+                .AsQueryable();
+
+            return query;
+        }
+
+        public IQueryable<TEntity> GetAsNoTracking() => Get().AsNoTracking();
+
+        public void Add(TEntity entity) => _context.Add(entity);
+
+        public void Update(TEntity entity) => _context.Entry(entity).State = EntityState.Modified;
+
+        public void Remove(Guid id) => _context.Remove(id);
+    }
+}
---
+++ CQRS.Core/Infrastructure/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using CQRS.Core.Domain;
+using System;
+using System.Linq;
+
+namespace CQRS.Core.Infrastructure
+{
+    public interface IGenericRepository<TEntity> where TEntity : Entity
+    {
+        IUnitOfWork UnitOfWork { get; }
+
+        void Add(TEntity entity);
+        IQueryable<TEntity> Get();
+        IQueryable<TEntity> GetAsNoTracking();
+        void Remove(Guid id);
+        void Update(TEntity entity);
+    }
+}

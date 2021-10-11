5b7a749bddd4f79ef29877710fbbc3ced7b6d73f
Túlio Paim <tutpaim@gmail.com>
2021-10-11 19:15:06 -0300

    Cria KafkaExtensions apra abstrair desserialização da mensagem

---
+++ CQRS.Core/Infrastructure/Kafka/KafkaExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace CQRS.Core.Infrastructure.Kafka
+{
+    public static class KafkaExtensions
+    {
+        public static PlatformMessage GetMessage(
+            this IConsumer<string, string> consumer,
+            CancellationToken cancellationToken)
+        {
+            var consumerResult = consumer.Consume(cancellationToken);
+
+            return JsonSerializer.Deserialize<PlatformMessage>(consumerResult.Message.Value);
+        }
+
+        public static TEvent GetEvent<TEvent>(this PlatformMessage platformMessage)
+            => JsonSerializer.Deserialize<TEvent>(platformMessage.Data);
+    }
+}

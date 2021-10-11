310bd0f8674b62e9e856e5e8837a871e85de5f97
Túlio Paim <tutpaim@gmail.com>
2021-10-11 09:10:45 -0300

    Cria console de benchmark

---
+++ CQRS.Console/BenchmarkHandleMediatorResult.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using CQRS.Core.API;
+using CQRS.Core.Application;
+
+namespace CQRS.BenchmarkConsole
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class BenchmarkHandleMediatorResult
+    {
+        private readonly List<ClasseDeResultado> _listaDeResultados = new();
+
+        private readonly ClasseDeResultado _resultado = new()
+        {
+            Id = Guid.NewGuid(),
+            Nome = "Pessoa"
+        };
+
+        public BenchmarkHandleMediatorResult()
+        {
+            for (var i = 0; i < 500; i++)
+            {
+                _listaDeResultados.Add(new ClasseDeResultado
+                {
+                    Id = Guid.NewGuid(),
+                    Nome = $"Pessoa {i}"
+                });
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Handle500MediatorResultsComReflecion()
+        {
+            foreach (var resultado in _listaDeResultados)
+            {
+                _ = resultado.HandleComReflection();
+            }
+        }
+
+        [Benchmark]
+        public void Handle500MediatorResultsSemReflecion()
+        {
+            foreach (var resultado in _listaDeResultados)
+            {
+                _ = resultado.HandleSemReflection();
+            }
+        }
+
+        [Benchmark]
+        public void Handle1MediatorResultComReflecion()
+        {
+            _ = _resultado.HandleComReflection();
+        }
+
+        [Benchmark]
+        public void Handle1MediatorResultSemReflecion()
+        {
+
+            _ = _resultado.HandleSemReflection();
+        }
+
+        protected class ClasseDeResultado : MediatorResult
+        {
+            public Guid Id { get; set; }
+            public string Nome { get; set; }
+        }
+    }
+}
---
+++ CQRS.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace CQRS.BenchmarkConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<BenchmarkHandleMediatorResult>();
+        }
+    }
+}

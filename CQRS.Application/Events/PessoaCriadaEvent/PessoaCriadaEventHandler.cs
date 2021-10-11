6047efc91e102d19213d9734968cee81975f5c7c
Túlio Paim <tutpaim@gmail.com>
2021-10-11 10:06:24 -0300

    Implementa evento de PessoaCriada

---
+++ CQRS.Application/Events/PessoaCriadaEvent/PessoaCriadaEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CQRS.Core.Application;
+using Microsoft.Extensions.Logging;
+
+namespace CQRS.Application.Events.PessoaCriadaEvent
+{
+    public class PessoaCriadaEventHandler : IEventHandler<PessoaCriadaEventInput>
+    {
+        private readonly ILogger _logger;
+
+        public PessoaCriadaEventHandler(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Handle(PessoaCriadaEventInput @event, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Evento {nameof(PessoaCriadaEventHandler)} disparado!");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ CQRS.Application/Events/PessoaCriadaEvent/PessoaCriadaEventInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CQRS.Core.Application;
+
+namespace CQRS.Application.Events.PessoaCriadaEvent
+{
+    public class PessoaCriadaEventInput : EventInput
+    {
+        public PessoaCriadaEventInput(Guid pessoaId)
+        {
+            PessoaId = pessoaId;
+        }
+
+        public Guid PessoaId { get; private set; }
+    }
+}

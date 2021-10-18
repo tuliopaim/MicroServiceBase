403a84ab0136a5dafedf3922b19efe38dede5405
Túlio Paim <tutpaim@gmail.com>
2021-10-18 14:11:03 -0300

    Cria estrutura para Hateoas

---
+++ CQRS.Core/API/Hateoas/HLink.cs
@@ -0,0 +1,9 @@
+﻿namespace CQRS.Core.API.Hateoas
+{
+    public class HLink
+    {
+        public string Rel { get; set; }
+        public string Href { get; set; }
+        public string Method { get; set; }
+    }
+}
--- CQRS.Core/Application/QueryResult.cs
+++ CQRS.Core/Application/QueryResult.cs
@@ -1,6 +1,10 @@
-﻿namespace CQRS.Core.Application
+﻿using System.Collections.Generic;
+using CQRS.Core.API.Hateoas;
+
+namespace CQRS.Core.Application
 {
     public class QueryResult : MediatorResult
     {
+        public IEnumerable<HLink> Links { get; set; }
     }
 }

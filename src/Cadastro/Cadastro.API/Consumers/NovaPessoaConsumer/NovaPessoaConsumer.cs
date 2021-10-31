f642acae52ce21ec0bb0204ce9890b5707ce3ee4
Túlio Paim <tutpaim@gmail.com>
2021-10-31 18:03:33 -0300

    Loga evento de PessoaCriada

--- src/Cadastro/Cadastro.API/Consumers/NovaPessoaConsumer/NovaPessoaConsumer.cs
+++ src/Cadastro/Cadastro.API/Consumers/NovaPessoaConsumer/NovaPessoaConsumer.cs
@@ -5,16 +5,19 @@ using Confluent.Kafka;
 using CQRS.Core.API;
 using CQRS.Core.Infrastructure.Kafka;
 using CQRS.Core.Infrastructure.Kafka.KafkaEventTypes;
+using Microsoft.Extensions.Logging;
 
 namespace Cadastro.API.Consumers.NovaPessoaConsumer
 {
     public class NovaPessoaConsumer : IConsumerHandler
     {
         private readonly IConsumer<string, string> _consumer;
+        private readonly ILogger<NovaPessoaConsumer> _logger;
 
-        public NovaPessoaConsumer(IKafkaBroker kafkaBroker)
+        public NovaPessoaConsumer(IKafkaBroker kafkaBroker, ILogger<NovaPessoaConsumer> logger)
         {
             _consumer = kafkaBroker.GetConsumer(KafkaTopics.NovaPessoaTopic);
+            _logger = logger;
         }
 
         public async Task Handle(CancellationToken cancellationToken)
@@ -33,11 +36,11 @@ namespace Cadastro.API.Consumers.NovaPessoaConsumer
             }
         }
 
-        private static Task HandlePessoaCriada(PlatformMessage<PessoaEventTypes> platformMessage)
+        private Task HandlePessoaCriada(PlatformMessage<PessoaEventTypes> platformMessage)
         {
             var @event = platformMessage.GetEvent<NovaPessoaConsumerInput, PessoaEventTypes>();
             
-            Console.WriteLine($"Evento do {nameof(NovaPessoaConsumer)} recebido - PessoaId: [{@event.PessoaId}]");
+            _logger.LogDebug("{ConsumerInputType} - Recebido.", nameof(NovaPessoaConsumerInput));
 
             return Task.CompletedTask;
         }

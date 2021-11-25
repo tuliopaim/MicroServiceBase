f8f8f4e4cccc46fb898836bd6f5091d3a3868a00
Túlio Paim <tutpaim@gmail.com>
2021-11-25 01:37:57 -0300

    Instala pacote, referencia projeto e injeta servicos

--- src/Email/EmailSender/Program.cs
+++ src/Email/EmailSender/Program.cs
@@ -1,12 +1,32 @@
 using Core.API;
+using EmailSender.Domain;
+using EmailSender.Infrastructure;
+using EmailSender.Templates;
+using FluentEmail.MailKitSmtp;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IRazorViewToStringRenderer, RazorViewToStringRenderer>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IEmailSender, EmailSender.Infrastructure.EmailSender>();
+
+builder.Services.AddScoped<IRazorViewToStringRenderer, RazorViewToStringRenderer>();
+
+builder.Services.AddFluentEmail("msbase@gmail.com", "MSBase")
+    .AddMailKitSender(new SmtpClientOptions{
+        User = "084d75abdb31df",
+        Password = "2a0cd517212549",
+        Port = 2525,
+        Server = "smtp.mailtrap.io",
+        RequiresAuthentication = true,
+        UseSsl = false,
+    });
 
 builder.Services.RegistrarCore(new CoreSettings
 {

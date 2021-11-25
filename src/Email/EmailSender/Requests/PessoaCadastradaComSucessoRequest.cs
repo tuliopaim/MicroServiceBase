7ea723dc202055e7aa7f850ae718eaa85adfd6f7
Túlio Paim <tutpaim@gmail.com>
2021-11-25 01:38:09 -0300

    Cria controller e request para teste de email

---
+++ src/Email/EmailSender/Requests/PessoaCadastradaComSucessoRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EmailSender.Requests
+{
+    public class PessoaCadastradaComSucessoRequest
+    {
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+    }
+}

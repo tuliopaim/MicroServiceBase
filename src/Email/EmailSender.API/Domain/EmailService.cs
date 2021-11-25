35fb616798b37145ada64cadb4d4c6aa73b30a6e
Túlio Paim <tutpaim@gmail.com>
2021-11-25 15:34:12 -0300

    Busca assunto de email do appsettings

--- src/Email/EmailSender.API/Domain/EmailService.cs
+++ src/Email/EmailSender.API/Domain/EmailService.cs
@@ -1,4 +1,5 @@
-﻿using EmailSender.API.Infrastructure;
+﻿using Core.API;
+using EmailSender.API.Infrastructure;
 using EmailSender.Templates.Views.Emails.PessoaCadastradaComSucesso;
 
 namespace EmailSender.API.Domain
@@ -6,15 +7,19 @@ namespace EmailSender.API.Domain
     public class EmailService : IEmailService
     {
         private readonly IEmailSender _emailSender;
+        private readonly IEnvironment _environment;
 
-        public EmailService(IEmailSender emailSender)
+        public EmailService(IEmailSender emailSender, IEnvironment environment)
         {
             _emailSender = emailSender;
+            _environment = environment;
         }
 
         public async Task<bool> EnviarEmailPessoaCadastradaComSucesso(string nome, string email)
         {
-            var model = new PessoaCadastradaComSucessoViewModel(nome, email, "Usuario cadastrado com sucesso!");
+            var assunto = _environment["EmailAssuntos:PessoaCadastradaComSucesso"];
+
+            var model = new PessoaCadastradaComSucessoViewModel(nome, email, assunto);
 
             return await _emailSender.SendEmail(model);
         }

ece9f2e65fd9281e72df5730f037417f7ba79195
Túlio Paim <tutpaim@gmail.com>
2021-11-25 15:33:17 -0300

    Dispara mensagem de novo email direto do NovaPessoaCommandHandler

--- src/Cadastro/Cadastro.Application/Commands/NovaPessoaCommand/NovaPessoaCommandHandler.cs
+++ src/Cadastro/Cadastro.Application/Commands/NovaPessoaCommand/NovaPessoaCommandHandler.cs
@@ -1,20 +1,21 @@
-﻿using Cadastro.Application.Events.PessoaCriadaEvent;
-using Cadastro.Domain.Entities;
+﻿using Cadastro.Domain.Entities;
 using Cadastro.Domain.Repositories;
 using Core.Application.Commands;
-using Core.Application.Mediator;
+using Core.Infrastructure.Email;
+using Core.Infrastructure.Kafka;
+using Core.Infrastructure.Kafka.KafkaMessageTypes;
 
 namespace Cadastro.Application.Commands.NovaPessoaCommand
 {
     public class NovaPessoaCommandHandler : ICommandHandler<NovaPessoaCommandInput, NovaPessoaCommandResult>
     {
         private readonly IPessoaRepository _pessoaRepository;
-        private readonly IMediator _mediator;
+        private readonly IKafkaBroker _broker;
 
-        public NovaPessoaCommandHandler(IPessoaRepository pessoaRepository, IMediator mediator)
+        public NovaPessoaCommandHandler(IPessoaRepository pessoaRepository, IKafkaBroker kafkaBroker)
         {
             _pessoaRepository = pessoaRepository;
-            _mediator = mediator;
+            _broker = kafkaBroker;
         }
 
         public async Task<NovaPessoaCommandResult> Handle(NovaPessoaCommandInput command, CancellationToken cancellationToken)
@@ -24,10 +25,12 @@ namespace Cadastro.Application.Commands.NovaPessoaCommand
             _pessoaRepository.Add(pessoa);
 
             await _pessoaRepository.UnitOfWork.CommitAsync(cancellationToken);
-
-            var novaPessoaEvent = new PessoaCriadaEventInput(pessoa.Id);
-
-            await _mediator.Publish(novaPessoaEvent, cancellationToken);
+                        
+            await _broker.PublishAsync(
+                KafkaTopics.NovoEmail, 
+                EmailMessageTypes.EmailPessoaCadastradaComSucesso, 
+                new EmailPessoaCadastradaComSucessoMessage(pessoa.Email, pessoa.Nome),
+                cancellationToken);
 
             return new NovaPessoaCommandResult(pessoa.Id);
         }

6cb7a942aa51ce646711426e0378cfb03abb3385
Túlio Paim <tutpaim@gmail.com>
2021-11-25 01:35:58 -0300

    Cria EmailBaseViewModel

---
+++ src/Email/EmailSender.Templates/Views/Emails/EmailBaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EmailSender.Templates.Views.Emails;
+
+public abstract class EmailBaseViewModel
+{
+    public EmailBaseViewModel(string email, string assunto)
+    {
+        Email = email;
+        Assunto = assunto;  
+    }
+
+    public string Email { get; set; }
+    public string Assunto { get; set; }
+}

866299801007e134de12b5fd7fa6a8a9e73a78ef
tuliopaimlyncas <tulio.p@lyncas.net>
2022-03-12 11:33:01 -0300

    Refatora consumers para novo padrão

---
+++ src/Services/MSBase.EmailSender.API/Consumers/NovoEmailConsumerBackgroundService.cs
@@ -0,0 +1,49 @@
+﻿using MSBase.Core.Extensions;
+using MSBase.Core.Infrastructure.RabbitMq;
+using MSBase.Core.Infrastructure.RabbitMq.Messages;
+using MSBase.Core.Infrastructure.RabbitMq.Messages.Email;
+using MSBase.EmailSender.API.Domain;
+using RabbitMQ.Client.Events;
+
+namespace MSBase.EmailSender.API.Consumers;
+
+public class NovoEmailConsumerBackgroundService : RabbitMqConsumerBackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<NovoEmailConsumerBackgroundService> _logger;
+
+    public NovoEmailConsumerBackgroundService(
+        RabbitMqConnection rabbitMqConnection,
+        ILogger<NovoEmailConsumerBackgroundService> logger,
+        IServiceProvider serviceProvider) : base(rabbitMqConnection, logger, RoutingKeys.NovaAuditoria)
+    {
+        _serviceProvider = serviceProvider;
+        _logger = logger;
+    }
+
+    
+    protected override async Task<bool> HandleMessage(BasicDeliverEventArgs rabbitEventArgs, RabbitMessage message)
+    {
+        var mensagem = rabbitEventArgs.GetDeserializedMessage();
+
+        var task = mensagem switch
+        {
+            EmailPessoaCadastradaComSucessoMessage pessoaCadastradaComSucessoMessage =>
+                HandlePessoaCadastradaComSucesso(pessoaCadastradaComSucessoMessage),
+            _ => Task.FromResult(true)
+        };
+        
+        return await task;
+    }
+    
+    private async Task<bool> HandlePessoaCadastradaComSucesso(EmailPessoaCadastradaComSucessoMessage mensagem)
+    {
+        using var scope = _serviceProvider.CreateScope();
+        var emailService = scope.ServiceProvider.GetService<IEmailService>();
+
+        ArgumentNullException.ThrowIfNull(emailService, nameof(emailService));
+
+        return await emailService.EnviarEmailPessoaCadastradaComSucesso(mensagem.NomePessoa, mensagem.Destinatario);
+    }
+
+}

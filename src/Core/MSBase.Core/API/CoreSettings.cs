65c2bdcf0e08e8c9e9468ab2b7bae167d4417514
tuliopaimlyncas <tulio.p@lyncas.net>
2022-03-12 10:59:53 -0300

    Refatora Startups e CoreSettings

--- src/Core/MSBase.Core/API/CoreSettings.cs
+++ src/Core/MSBase.Core/API/CoreSettings.cs
@@ -1,67 +1,36 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 
-namespace MSBase.Core.API
+namespace MSBase.Core.API;
+
+public class CoreSettings
 {
-    public class CoreSettings
-    {
-        /// <summary>
-        /// Interface IConfiguration
-        /// </summary>
-        public IConfiguration Configuration { get; set; }
+    /// <summary>
+    /// Interface IConfiguration
+    /// </summary>
+    public IConfiguration Configuration { get; set; }
 
-        /// <summary>
-        /// Configurar ou não o Mediator
-        /// </summary>
-        public bool ConfigurarMediator { get; set; } = true;
+    /// <summary>
+    /// Configurar ou não o Mediator
+    /// </summary>
+    public bool ConfigurarMediator { get; set; } = true;
 
-        /// <summary>
-        /// Tipo da classe do startup do serviço.
-        /// </summary>
-        public Type TipoDoStartup { get; set; }
+    /// <summary>
+    /// Tipo da classe do startup do serviço.
+    /// </summary>
+    public Type TipoDoStartup { get; set; }
 
-        /// <summary>
-        /// Nome do assembly do projeto de aplicação, utilizado para injetar as dependências
-        /// </summary>
-        public Type TipoDaCamadaDeApplication { get; set; }
-
-        /// <summary>
-        /// Configurar o Exception Pipeline Behavior, para tratar exceptions que ocorram no Handler
-        /// </summary>
-        public bool ConfigurarExceptionPipelineBehavior { get; set; } = true;
-
-        /// <summary>
-        /// Configurar o Fail Fast Pipeline Behavior, para validar commands antes de chamar o Handler
-        /// </summary>
-        public bool ConfigurarFailFastPipelineBehavior { get; set; } = true;
-
-        /// <summary>
-        /// Interface IHostEnvironment, utilizada para a correta configuração da interface IEnvironment
-        /// </summary>
-        public IHostEnvironment HostEnvironment { get; set; }
-
-        /// <summary>
-        /// Configura a injeção da interface IEnvironment
-        /// Depende da configuração correta da propriedade IHostEnvironment e IConfiguration
-        /// </summary>
-        public bool ConfigureIEnvironment { get; set; } = true;
-
-        /// <summary>
-        /// Configura e instancia o Broker do Kafka 
-        /// </summary>
-        public bool ConfigurarKafkaBroker { get; set; } = true;
-
-        /// <summary>
-        /// Injeta todas as classes que implementam IConsumerHandler como Singleton
-        /// e dispara em Tasks separadas o Handle() de cada uma.
-        /// Caso aconteça algum erro não tratado a task é disparada novamente
-        /// </summary>
-        public bool ConfigurarConsumerHandlers { get; set; } = true;
-
-        /// <summary>
-        /// Injetar IHateoasHelper
-        /// </summary>
-        public bool ConfigurarHateoasHelper { get; set; } = true;
-
-    }
+    /// <summary>
+    /// Nome do assembly do projeto de aplicação, utilizado para injetar as dependências
+    /// </summary>
+    public Type TipoDaCamadaDeApplication { get; set; }
+        
+    /// <summary>
+    /// Configura e instancia RabbitMqConnection e RabbitMqProducer 
+    /// </summary>
+    public bool ConfigurarRabbitMq { get; set; } = true;    
+        
+    /// <summary>
+    /// Injetar IHateoasHelper
+    /// </summary>
+    public bool ConfigurarHateoasHelper { get; set; } = true;
 }
--- src/Core/MSBase.Core/API/Startup.cs
+++ src/Core/MSBase.Core/API/Startup.cs
@@ -1,168 +1,112 @@
-﻿using System.Reflection;
-using FluentValidation;
+﻿using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using MSBase.Core.API.Hateoas;
 using MSBase.Core.Application.Mediator.Pipeline;
+using MSBase.Core.Infrastructure.RabbitMq;
 using Serilog;
 
-namespace MSBase.Core.API
+namespace MSBase.Core.API;
+
+public static class Startup
 {
-    public static class Startup
+    public static IServiceCollection RegistrarCore(this IServiceCollection services, CoreSettings settings)
     {
-        public static IServiceCollection RegistrarCore(this IServiceCollection services, CoreSettings settings)
-        {
-            services
-                .AddSingleton(settings)
-                .RegistrarApi(settings)
-                .RegistrarApplication(settings)
-                .RegistrarCrossCutting(settings);
+        services
+            .AddSingleton(settings)
+            .RegistrarApi(settings)
+            .RegistrarApplication(settings)
+            .RegistrarInfrastructure(settings);
 
-            return services;
+        return services;
+    }
+
+    private static IServiceCollection RegistrarApi(this IServiceCollection services, CoreSettings settings)
+    {
+        if (settings.ConfigurarHateoasHelper)
+        {
+            services.AddScoped<IHateoasHelper, HateoasHelper>();
         }
 
-        private static IServiceCollection RegistrarApi(this IServiceCollection services, CoreSettings settings)
+        services.RegistrarLogging(settings);
+
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        services.AddSingleton<IEnvironment, Environment>();
+
+        return services;
+    }
+
+    private static IServiceCollection RegistrarLogging(this IServiceCollection services, CoreSettings settings)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(settings.Configuration)
+            .Enrich.FromLogContext()
+            .Enrich.WithEnvironmentName()
+            .CreateLogger();
+
+        services.AddLogging(x => x.AddSerilog());
+
+        return services;
+    }
+
+    private static IServiceCollection RegistrarApplication(this IServiceCollection services, CoreSettings settings)
+    {
+        if (!settings.ConfigurarMediator) return services;
+
+        services.AddMediatR(settings.TipoDoStartup);
+
+        services.AddScoped<Application.Mediator.IMediator, Application.Mediator.Mediator>();
+                
+        services
+            .RegistrarExceptionPipelineBehavior()
+            .RegistrarLogPipelineBehavior()
+            .RegistrarFailFastPipeline(settings)
+            .RegistrarValidators(settings);
+
+        return services;
+    }
+        
+    private static IServiceCollection RegistrarExceptionPipelineBehavior(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ExceptionPipelineBehavior<,>));
+
+        return services;
+    }
+
+    private static IServiceCollection RegistrarLogPipelineBehavior(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LogPipelineBehavior<,>));
+
+        return services;
+    }
+
+    private static IServiceCollection RegistrarFailFastPipeline(this IServiceCollection services, CoreSettings settings)
+    {
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(FailFastPipelineBehavior<,>));
+
+        return services;
+    }
+
+    private static IServiceCollection RegistrarValidators(this IServiceCollection services, CoreSettings settings)
+    {
+        var assembly = settings.TipoDaCamadaDeApplication.Assembly;
+
+        foreach (var validator in AssemblyScanner.FindValidatorsInAssembly(assembly))
         {
-            if (settings.ConfigurarConsumerHandlers)
-            {
-                RegisterConsumers(services, settings);
-            }
-
-            if (settings.ConfigurarHateoasHelper)
-            {
-                services.AddScoped<IHateoasHelper, HateoasHelper>();
-            }
-
-            services.RegistrarLogging(settings);
-
-            return services;
+            services.AddScoped(validator.InterfaceType, validator.ValidatorType);
         }
-
-        private static IServiceCollection RegistrarLogging(this IServiceCollection services, CoreSettings settings)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(settings.Configuration)
-                .Enrich.FromLogContext()
-                .Enrich.WithEnvironmentName()
-                .CreateLogger();
-
-            services.AddLogging(x => x.AddSerilog());
-
-            return services;
-        }
-
-        private static void RegisterConsumers(IServiceCollection services, CoreSettings settings)
-        {
-            services.AddHostedService<RunConsumersService>();
-
-            var consumersDoAssembly = settings.TipoDoStartup.Assembly
-                .ExportedTypes
-                .Select(t => t.GetTypeInfo())
-                .Where(t => t.IsClass && !t.IsAbstract && t.IsAssignableTo(typeof(IConsumerHandler)))
-                .ToList();
-
-            foreach (var consumerDoAssembly in consumersDoAssembly)
-            {
-                services.AddSingleton(consumerDoAssembly.AsType());
-            }
-        }
-
-        private static IServiceCollection RegistrarApplication(this IServiceCollection services, CoreSettings settings)
-        {
-            if (settings.ConfigurarMediator)
-            {
-                services.AddMediatR(settings.TipoDoStartup);
-
-                services.AddScoped<Application.Mediator.IMediator, Application.Mediator.Mediator>();
-
-                //services.RegistrarMediatorHandlers(settings);
-
-                if (settings.ConfigurarExceptionPipelineBehavior)
-                {
-                    services.RegistrarExceptionPipelineBehavior();
-                }
-
-                services.RegistrarLogPipelineBehavior();
-
-                if (settings.ConfigurarFailFastPipelineBehavior)
-                {
-                    services.RegistrarFailFastPipeline(settings);
-                }
-            }
-
-            if (settings.ConfigurarKafkaBroker)
-            {
-                services.AddSingleton<IKafkaBroker, KafkaBroker>();
-            }
-
-            return services;
-        }
-
-        private static void RegistrarMediatorHandlers(this IServiceCollection services, CoreSettings settings)
-        {
-            var assembly = settings.TipoDaCamadaDeApplication.Assembly;
-
-            var classesDaApplication = assembly.ExportedTypes
-                .Select(t => t.GetTypeInfo())
-                .Where(t => t.IsClass && !t.IsAbstract);
             
-            var nomeDasInterfacesHandlers = new List<string>
-            {
-                typeof(IRequestHandler<,>).Name,
-                typeof(INotificationHandler<>).Name
-            };
+        return services;
+    }
 
-            foreach (var tipo in classesDaApplication)
-            {
-                var interfacesDosHandlers = tipo.ImplementedInterfaces
-                    .Select(i => i.GetTypeInfo())
-                    .Where(i => nomeDasInterfacesHandlers.Contains(i.Name));
+    private static IServiceCollection RegistrarInfrastructure(this IServiceCollection services, CoreSettings settings)
+    {
+        if (!settings.ConfigurarRabbitMq) return services;
 
-                foreach (var interfaceDoHandler in interfacesDosHandlers)
-                {
-                    services.AddTransient(interfaceDoHandler.AsType(), tipo.AsType());
-                }
-            }
-        }
+        services.AddSingleton<RabbitMqConnection>();
+        services.AddScoped<RabbitMqProducer>();
 
-        private static void RegistrarExceptionPipelineBehavior(this IServiceCollection services)
-        {
-            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ExceptionPipelineBehavior<,>));
-        }
-
-        private static void RegistrarLogPipelineBehavior(this IServiceCollection services)
-        {
-            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LogPipelineBehavior<,>));
-        }
-
-        private static void RegistrarFailFastPipeline(this IServiceCollection services, CoreSettings settings)
-        {
-            services.RegistrarValidators(settings);
-
-            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(FailFastPipelineBehavior<,>));
-        }
-
-        private static void RegistrarValidators(this IServiceCollection services, CoreSettings settings)
-        {
-            var assembly = settings.TipoDaCamadaDeApplication.Assembly;
-
-            foreach (var validator in AssemblyScanner.FindValidatorsInAssembly(assembly))
-            {
-                services.AddScoped(validator.InterfaceType, validator.ValidatorType);
-            }
-        }
-
-        private static IServiceCollection RegistrarCrossCutting(this IServiceCollection services, CoreSettings settings)
-        {
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-            if (settings.ConfigureIEnvironment)
-            {
-                services.AddSingleton<IEnvironment, Environment>();
-            }
-
-            return services;
-        }
+        return services;
     }
 }
